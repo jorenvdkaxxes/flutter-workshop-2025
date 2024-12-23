5fec9224c4d83ba9e036a4fca14085cf21ca481d
JorenVdk <joren.vandekerckhove@axxes.com>
2024-12-23 22:12:19 +0100

    Small change

--- simply_lifestyle_app/lib/main.dart
+++ simply_lifestyle_app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Simply Lifestyle App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
