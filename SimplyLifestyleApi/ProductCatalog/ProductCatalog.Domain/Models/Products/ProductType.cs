9df245d9fac854dba9761d7f6b96d72321dd2c11
JorenVdk <joren.vandekerckhove@axxes.com>
2024-12-25 14:38:19 +0100

    Small change

--- SimplyLifestyleApi/ProductCatalog/ProductCatalog.Domain/Models/Products/ProductType.cs
+++ SimplyLifestyleApi/ProductCatalog/ProductCatalog.Domain/Models/Products/ProductType.cs
@@ -1,15 +1,13 @@
 public class ProductType : Enumeration
 {
-    public static ProductType Electronics = new(1, nameof(Electronics));
-    public static ProductType Clothing = new(2, nameof(Clothing));
-    public static ProductType Books = new(3, nameof(Books));
-    public static ProductType HomeAppliances = new(4, nameof(HomeAppliances));
+    public static ProductType KaliSeats = new(1, nameof(KaliSeats));
+    public static ProductType FabricSeats = new(2, nameof(FabricSeats));
 
-    private ProductType(int value, string name) 
+    private ProductType(int value, string name)
         : base(value, name)
     {
     }
-    
+
     private ProductType(int value)
         : this(value, FromValue<ProductType>(value).Name)
     {
