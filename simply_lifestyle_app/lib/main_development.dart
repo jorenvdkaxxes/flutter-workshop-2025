50c62a7b85e897897dccb3ad8536637f8f4e9992
JorenVdk <joren.vandekerckhove@axxes.com>
2025-03-31 11:34:59 +0200

    Updated the settings to use more the API and also config the app to POST to the API

--- simply_lifestyle_app/lib/main_development.dart
+++ simply_lifestyle_app/lib/main_development.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
 import 'package:provider/provider.dart';
@@ -5,15 +7,25 @@ import 'package:provider/provider.dart';
 import 'config/dependencies.dart';
 import 'main.dart';
 
+// Only use this during development, it prevents the client handshake error
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 /// Development config entry point.
 /// Launch with `flutter run --target lib/main_development.dart`.
 /// Uses local data.
 void main() {
   Logger.root.level = Level.ALL;
+  HttpOverrides.global = MyHttpOverrides();
 
   runApp(
     MultiProvider(
-      providers: providersLocal,
+      providers: providersRemote,
       child: const MyApp(),
     ),
   );
