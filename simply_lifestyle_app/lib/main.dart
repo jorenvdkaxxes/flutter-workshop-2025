d0b98cd4c1a5011f4427f6dabef8233f6d9ec013
jorenvandekerckhove <vdk.joren@hotmail.com>
2025-04-13 17:56:10 +0200

    Fixed import

--- simply_lifestyle_app/lib/main.dart
+++ simply_lifestyle_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 import 'package:simply_lifestyle_app/routing/router.dart';
 import 'package:simply_lifestyle_app/routing/routes.dart';
 import 'package:simply_lifestyle_app/ui/core/localization/applocalization.dart';
